7c85d60ff5addcabab4c0b7a32ecbdc675acf251
Umutjan Mahmut <umutjan2010@hotmail.com>
2021-05-09 20:40:20 -0700

    remove useless dependencies to make gatsby build work

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,25 +28,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Oswald`,
-            subsets: [`latin`],
-          },
-          {
-            family: `Raleway`,
-            variants: [`400`, `500`, `700`],
-          },
-          {
-            family: `Yusei Magic`,
-            variants: [`400`],
-          },
-        ],
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
